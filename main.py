407d865026c19aaebe910b7e82f9aaa88c5eb6e5
nikhil <nikhilanthonyhenry@gmail.com>
2019-04-14 20:54:11 +0530

    add encrypt and decrypt option to files

--- main.py
+++ main.py
@@ -27,6 +27,17 @@ def encrypt(message):
 
     print("Message encrypted succesfully")
 
+def encrypt_file(file_name):
+    f = Fernet(getKey())
+    with open(file_name,'rb') as file:
+        encrypted_file =f.encrypt(file.read())
+    file.close()
+
+    write_file_name = file_name.split(".")[0]
+    write_file = open("%s.enc"%(write_file_name),"wb")
+    write_file.write(encrypted_file)
+    write_file.close()
+
 def decrypt(file_name):
     try:
         file=open(file_name,"rb")
@@ -35,8 +46,12 @@ def decrypt(file_name):
         f = Fernet(getKey())
         decrypted_message=f.decrypt(encrypted_message)
         message = decrypted_message.decode()
-        print(message)
-    except:
+
+        write_file_name = 'decrypted_'+file_name.split(".")[0]+".txt"
+        write_file = open(write_file_name,"w")
+        write_file.write(message)
+        write_file.close()
+    except :
         print("Incorrect Key")
 
 
@@ -47,6 +62,8 @@ if(args!=[]):
         generate_key()
     elif(args[0].lower()  == 'encrypt'):
         encrypt(args[1])
+    elif(args[0].lower() == 'encrypt_file'):
+        encrypt_file(args[1])
     elif(args[0].lower() == 'decrypt'):
         decrypt(args[1])
 else:
